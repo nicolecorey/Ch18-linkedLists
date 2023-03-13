1c97abd2087585ef635149156299ab80493ec3cc
mybil <mybil@NICOLE>
2023-03-13 10:18:22 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿public class Example
+{
+    public static void Main()
+    {
+        // Create the link list.
+        string[] family =
+            { "Peter", "Louis", "Meg", "Stewie", "Chris"};
+        LinkedList<string> familyList = new LinkedList<string>(family);
+        Console.WriteLine("Griffin Family:");
+        foreach (string familyMem in familyList)
+        {
+            Console.WriteLine(familyMem);
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("The first family member in the list is: {0}", familyList.First.Value);
+        Console.WriteLine("The last family member in the list is: {0}", familyList.Last.Value);
+        Console.WriteLine();
+
+        
+        LinkedListNode<string> targetLocation = familyList.Find("Louis");
+        familyList.AddAfter(targetLocation, "Brian");
+        Console.WriteLine("Full Griffin Family");
+        foreach (string familyMem in familyList)
+        {
+            Console.WriteLine(familyMem);
+        }
+        Console.WriteLine();
+        
+        familyList.RemoveLast();
+
+        Console.WriteLine("Updated Griffin Family");
+        Console.WriteLine("There are now {0} members in the Griffin family.", familyList.Count);
+        foreach (string familyMem in familyList)
+        {
+            Console.WriteLine(familyMem);
+        }
+    }
+
+}
